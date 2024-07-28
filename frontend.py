ac7125d7d19da63e9eafd14c9d658b05f9ec5ffa
vishal_s_02 <svs15324@gmail.com>
2024-07-28 10:04:36 +0530

    refine-03

--- frontend.py
+++ frontend.py
@@ -25,20 +25,21 @@ save_path = st.text_input("Enter the path to save the downloaded file:", value=d
 
 
 if st.button(label="Download"):
-    st.write(url)
-    st.write(save_path)
-    st.write(download_type)
-    try:
-        if download_type == 'Download Video':
-            download_single_video(url, save_path)
-        elif download_type == 'Download Playlist':
-            download_playlist(url, save_path)
-        elif download_type == 'Download Video as Audio':
-            download_video_as_audio(url, save_path)
-        elif download_type == 'Download Playlist as MP3':
-            download_playlist_as_audio(url, save_path)
+    if url and save_path:
+        st.header("Downloading Video...")
+        try:
+            if download_type == 'Download Video':
+                download_single_video(url, save_path)
+            elif download_type == 'Download Playlist':
+                download_playlist(url, save_path)
+            elif download_type == 'Download Video as Audio':
+                download_video_as_audio(url, save_path)
+            elif download_type == 'Download Playlist as MP3':
+                download_playlist_as_audio(url, save_path)
 
-        st.write('Download started! Check your downloads folder.')
-    except Exception as e:
-        st.write(f'Error: {str(e)}')
-        print("Error details:", str(e))
+            st.success('Download started! Check your downloads folder.')
+        except Exception as e:
+            st.write(f'Error: {str(e)}')
+            print("Error details:", str(e))
+    else:
+        st.error("Please enter Valid Url or Path")
